582245f5fbd7eeac991d7f8e08cbdf5e1463294a
emutombene <emutombene@bol.com>
2023-07-12 05:56:11 +0200

    refactor

--- recipe/src/test/groovy/RecipeApiIntegrationSpec.groovy
+++
@@ -1,9 +0,0 @@
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.test.web.servlet.MockMvc
-import spock.lang.Subject
-
-class RecipeApiIntegrationSpec extends BaseSpeificationTest{
-
-    @Autowired
-    private MockMvc mvcCon
-}
--- user/src/main/java/edson/mutombene/user/UserRegistrationRequest.java
+++
@@ -1,7 +0,0 @@
-package edson.mutombene.user;
-
-public record UserRegistrationRequest(
-        String firstName,
-        String lastName,
-        String email) {
-}
